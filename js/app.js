674140bc806fb6737069d6b11f0918ee4fc47540
Daniel Ugarov <tyradire@yandex.ru>
2023-04-22 16:38:34 +0300

    init commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+(() => {
+  // src/client/js/app.js
+  var App = class {
+    constructor() {
+      console.log("app code starts here");
+    }
+  };
+  window.addEventListener("DOMContentLoaded", () => {
+    console.log("app.js dom ready");
+    new App();
+  });
+})();
+//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsiLi4vLi4vLi4vc3JjL2NsaWVudC9qcy9hcHAuanMiXSwKICAic291cmNlc0NvbnRlbnQiOiBbImV4cG9ydCBkZWZhdWx0IGNsYXNzIEFwcCB7XHJcblx0Y29uc3RydWN0b3IoKSB7XHJcblx0XHRjb25zb2xlLmxvZygnYXBwIGNvZGUgc3RhcnRzIGhlcmUnKVxyXG5cdH1cclxufVxyXG5cclxud2luZG93LmFkZEV2ZW50TGlzdGVuZXIoJ0RPTUNvbnRlbnRMb2FkZWQnLCAoKSA9PiB7XHJcbiAgICBjb25zb2xlLmxvZygnYXBwLmpzIGRvbSByZWFkeScpO1xyXG5cdG5ldyBBcHAoKTtcclxufSk7XHJcbiJdLAogICJtYXBwaW5ncyI6ICI7O0FBQUEsTUFBcUIsTUFBckIsTUFBeUI7QUFBQSxJQUN4QixjQUFjO0FBQ2IsY0FBUSxJQUFJLHNCQUFzQjtBQUFBLElBQ25DO0FBQUEsRUFDRDtBQUVBLFNBQU8saUJBQWlCLG9CQUFvQixNQUFNO0FBQzlDLFlBQVEsSUFBSSxrQkFBa0I7QUFDakMsUUFBSSxJQUFJO0FBQUEsRUFDVCxDQUFDOyIsCiAgIm5hbWVzIjogW10KfQo=
